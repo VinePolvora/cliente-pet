c92977eb39fe9fa26c1ceb33eb08a30f52c41516
unknown <santosvinepolvora@gmail.com>
2022-11-17 18:50:14 -0300

    implementa fluxo do repository

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetClienteListResponse.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetClienteListResponse.java
@@ -1,5 +1,12 @@
 package br.com.petz.clientepet.pet.application.api;
 
+import java.util.List;
+import java.util.UUID;
+
 public class PetClienteListResponse {
+
+	public static List<PetClienteListResponse> converte(UUID idCliente) {
+		return null;
+	}
 
 }
